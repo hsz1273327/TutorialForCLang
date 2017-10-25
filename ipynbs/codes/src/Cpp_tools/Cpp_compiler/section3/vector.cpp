4cb1bb018fea804d7fd9c753df4d74c0f5c61868
HUANG SIZHE <hsz1273327@gmail.com>
2017-10-25 18:33:21 +0800

    update

---
+++ ipynbs/codes/src/Cpp_tools/Cpp_compiler/section3/vector.cpp
@@ -0,0 +1,46 @@
+#include <cmath>
+#include "vector.hpp"
+Vector::BinaryVector::BinaryVector(){
+    x=0.0;
+    y=0.0;
+}
+                
+Vector::BinaryVector::BinaryVector(float in_x,float in_y){
+    x=in_x;
+    y=in_y;
+}
+Vector::BinaryVector::BinaryVector(int in_x,int in_y){
+    x=static_cast<float>(in_x);
+    y=static_cast<float>(in_y);
+}
+Vector::BinaryVector::BinaryVector(int in_x,float in_y){
+    x=static_cast<float>(in_x);
+    y=in_y;
+}
+Vector::BinaryVector::BinaryVector(float in_x,int in_y){
+    x=in_x;
+    y=static_cast<float>(in_y);
+}
+                
+float Vector::BinaryVector::mod(){
+    float result = sqrt(x*x+y*y);
+    return result;
+}
+float Vector::BinaryVector::mul(const Vector::BinaryVector & that_vector){
+    float result = x*taht_vector.x+y*taht_vector.y;
+    return result
+}
+                
+    
+Vector::BinaryVector Vector::BinaryVector::add(const Vector::BinaryVector & that_vector){
+    Vector::BinaryVector result(x+that_vector.x,y+that_vector.y);
+    return result;
+    
+}
+                            
+float Vector::BinaryVector::operator*(const Vector::BinaryVector& that_vector){
+    return mul(that_vector)
+}
+Vector::BinaryVector Vector::BinaryVector::operator+(const Vector::BinaryVector& that_vector){
+    return add(that_vector)
+}
---
+++ ipynbs/codes/test/Cpp_tools/Cpp_compiler/section3/test.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "vector.hpp"
+
+using Vector;
+
+int main(void){
+    BinaryVector A(1,2),B(3,4);
+    printf("mod(A)=%f\n",A.mod());
+    printf("mod(B)=%f\n",B.mod);
+    printf("mul(A,B)=%f\n",A*B);
+    BinaryVector result = A+B;
+    printf("add(A,B)=<%f,%f>\n",result.x,result.y);
+}
