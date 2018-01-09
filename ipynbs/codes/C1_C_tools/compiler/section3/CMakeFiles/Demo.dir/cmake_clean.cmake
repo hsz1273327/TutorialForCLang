14e9b28889ba3999f48a2d448dde5c5e31a36c03
HUANG SIZHE <hsz1273327@gmail.com>
2018-01-10 02:47:57 +0800

    update

---
+++ ipynbs/codes/C1_C_tools/compiler/section3/CMakeFiles/Demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Demo.dir/test/test.c.o"
+  "bin/Demo.pdb"
+  "bin/Demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- ipynbs/工具链/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['c的工具链',['C的工具链',['../md__r_e_a_d_m_e.html',1,'']]]
-];
