138e13edfdfa0f116dbee7be6668e0fabf24e8cd
HUANG SIZHE <hsz1273327@gmail.com>
2021-03-17 17:36:57 +0800

    update

--- examples/工具链/编译器/S3/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/huangsizhe/Workspace/Documents/TutorialForCLang/c-tool-chain/code/compiler/S3
-# Build directory: /Users/huangsizhe/Workspace/Documents/TutorialForCLang/c-tool-chain/code/compiler/S3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(DemoRuns "/Users/huangsizhe/Workspace/Documents/TutorialForCLang/c-tool-chain/code/compiler/S3/build/bin/Demo")
-set_tests_properties(DemoRuns PROPERTIES  _BACKTRACE_TRIPLES "/Users/huangsizhe/Workspace/Documents/TutorialForCLang/c-tool-chain/code/compiler/S3/CMakeLists.txt;20;add_test;/Users/huangsizhe/Workspace/Documents/TutorialForCLang/c-tool-chain/code/compiler/S3/CMakeLists.txt;0;")
