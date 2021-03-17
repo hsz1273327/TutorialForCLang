389eb32eb03a2078074ebbd9be55c563e7dc28d7
HUANG SIZHE <hsz1273327@gmail.com>
2021-03-17 19:44:43 +0800

    update

---
+++ examples/工具链/测试工具/simpletest/CMakeFiles/vectorTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vectorTest.dir/test/test.c.o"
+  "build/test/vectorTest"
+  "build/test/vectorTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/vectorTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ examples/工具链/测试工具/simpletest/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/TutorialForCLang/examples/工具链/测试工具/simpletest
+# Build directory: /workspaces/TutorialForCLang/examples/工具链/测试工具/simpletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(TestRuns "/workspaces/TutorialForCLang/examples/工具链/测试工具/simpletest/build/test/vectorTest")
+set_tests_properties(TestRuns PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/TutorialForCLang/examples/工具链/测试工具/simpletest/CMakeLists.txt;20;add_test;/workspaces/TutorialForCLang/examples/工具链/测试工具/simpletest/CMakeLists.txt;0;")
