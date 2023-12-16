e8dad1c7068f330ac8542ece6d1322ce704aba27
HUANG SIZHE <hsz1273327@gmail.com>
2023-12-16 14:48:49 +0800

    Update testquery.py

--- 与Python交互/C程序中嵌入python解释器/helloworldserv/testquery.py
+++ 与Python交互/C程序中嵌入python解释器/helloworldserv/testquery.py
@@ -1,10 +1,5 @@
 import requests as rq
 
-# with open("testscript.py", "rb") as f:
-#     code = f.read()
-# rs = rq.post("http://localhost:18080/submit",json={
-#     "code": code
-# })
 rs = rq.post("http://localhost:18080/submit", files=[
     ('script', ('testscript.py', open('testscript.py', 'rb'), 'application/python'))])
 print(rs.status_code)
